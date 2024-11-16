986431ceeea119602ec7f1608bc8bdce3a70461a
MohamedHamouch <med10hamouch@gmail.com>
2024-11-17 00:42:15 +0100

    count book in cart icon

--- script/cart.js
+++ script/cart.js
@@ -17,67 +17,68 @@ function displayCart() {
         emptyMessage.textContent = "Your Cart is empty, Add items from shop.";
         emptyMessage.className = "text-center text-gray-500 mt-16 mb-8 md:mb-12 text-lg md:text-2xl font-semibold";
         booksContainer.appendChild(emptyMessage);
-        return;
-    }
 
-    cartData.forEach(book => {
-        const cartBook = document.createElement('div');
-        cartBook.className = "grid grid-cols-[50%,auto,auto,auto,3%] items-center border-b p-2 gap-2 sm:gap-4 hover:bg-gray-100";
-        cartBook.innerHTML = `
-                         <div class="flex items-center gap-2">
-                            <img src="${book.img}" alt="${book.title}"
-                                class="w-12 h-16 object-contain">
-                            <span class="text-gray-800">${book.title}</span>
-                        </div>
+    } else {
+        cartData.forEach(book => {
+            const cartBook = document.createElement('div');
+            cartBook.className = "grid grid-cols-[50%,auto,auto,auto,3%] items-center border-b p-2 gap-2 sm:gap-4 hover:bg-gray-100";
+            cartBook.innerHTML = `
+                             <div class="flex items-center gap-2">
+                                <img src="${book.img}" alt="${book.title}"
+                                    class="w-12 h-16 object-contain">
+                                <span class="text-gray-800">${book.title}</span>
+                            </div>
+    
+                            <!-- prix -->
+                            <div class="text-gray-700">$${book.price}</div>
+    
+                            <!-- quantity -->
+                            <div>
+                                <input type="number" class="quantity-input w-12 text-center border rounded" value="${book.quantity}" min="1">
+                            </div>
+    
+                            <!-- subtotal -->
+                            <div class="subtotal text-gray-700">$${book.price * book.quantity}</div>
+    
+                            <!-- delete icon -->
+                            <button class="trash-icon">
+                                <i class="fa-solid fa-trash" style="color: #ffc857;"></i>
+                            </button>
+                             `
+            booksContainer.appendChild(cartBook);
 
-                        <!-- prix -->
-                        <div class="text-gray-700">$${book.price}</div>
-
-                        <!-- quantity -->
-                        <div>
-                            <input type="number" class="quantity-input w-12 text-center border rounded" value="${book.quantity}" min="1">
-                        </div>
-
-                        <!-- subtotal -->
-                        <div class="subtotal text-gray-700">${book.price * book.quantity}</div>
-
-                        <!-- delete icon -->
-                        <button class="trash-icon">
-                            <i class="fa-solid fa-trash" style="color: #ffc857;"></i>
-                        </button>
-                         `
-        booksContainer.appendChild(cartBook);
-
-        const removeIcon = cartBook.querySelector('.trash-icon');
-        removeIcon.addEventListener('click', () => {
-            cartBook.remove();
-            const bookIndex = cartData.findIndex(currentBook => currentBook.title === book.title);
-            if (bookIndex !== -1) {
-                cartData.splice(bookIndex, 1);
-                localStorage.setItem('card', JSON.stringify(cartData));
-                if (cartData.length === 0) {
-                    displayCart();
+            const removeIcon = cartBook.querySelector('.trash-icon');
+            removeIcon.addEventListener('click', () => {
+                cartBook.remove();
+                const bookIndex = cartData.findIndex(currentBook => currentBook.title === book.title);
+                if (bookIndex !== -1) {
+                    cartData.splice(bookIndex, 1);
+                    localStorage.setItem('card', JSON.stringify(cartData));
+                    if (cartData.length === 0) {
+                        displayCart();
+                    }
                 }
-            }
+
+            });
+            console.log(typeof book.quantity)
+
+            const newQuantity = cartBook.querySelector('.quantity-input')
+            newQuantity.addEventListener('input', () => {
+
+                if (Number(newQuantity.value) > 0) {
+                    book.quantity = Number(newQuantity.value);
+                    console.log(typeof book.quantity)
+                    const subtotal = cartBook.querySelector('.subtotal')
+                    subtotal.textContent = `$${book.price * book.quantity}`;
+                    localStorage.setItem('card', JSON.stringify(cartData));
+                } else {
+                    newQuantity.value = book.quantity
+                }
+            });
 
         });
-        console.log(typeof book.quantity)
-
-        const newQuantity = cartBook.querySelector('.quantity-input')
-        newQuantity.addEventListener('input', () => {
-
-            if (Number(newQuantity.value) > 0) {
-                book.quantity = Number(newQuantity.value);
-                console.log(typeof book.quantity)
-                const subtotal =  cartBook.querySelector('.subtotal')
-                subtotal.textContent = `$${book.price * book.quantity}`;
-                localStorage.setItem('card', JSON.stringify(cartData));
-            }else{
-                newQuantity.value = book.quantity
-            }
-        });
-
-    });
+    }
+    countbook();
 }
 
 emptyBtn.addEventListener('click', () => {
@@ -87,3 +88,9 @@ emptyBtn.addEventListener('click', () => {
     displayCart();
     localStorage.setItem('card', JSON.stringify(cartData));
 });
+
+function countbook() {
+    let countbook = document.querySelector("#book-count")
+    countbook.innerHTML = cartData.length
+
+}
